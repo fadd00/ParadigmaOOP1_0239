1589b46bb068c07f15067b2692d7b1d20b708797
fadd00 <andhikahutama9@gmail.com>
2024-05-21 15:15:04 +0700

    membuat library jantung

---
+++ comp/jantung.h
@@ -0,0 +1,16 @@
+#ifdef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung{
+    public:
+    jantung(){
+        cout << "jantung hidup\n";
+    }
+    ~jantung(){
+        cout << "jantung mati\n";
+    }   
+};
+
+#endif
