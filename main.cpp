f2db8b0d44e54c560af27d006f0175e6408eaf9b
fadd00 <andhikahutama9@gmail.com>
2024-05-21 14:57:21 +0700

    membuat class mahasiswa

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+    private:
+    const int id;
+    string nama;
+    float nilai;
+
+    public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+
+    }
+    ~mahasiswa() {
+        cout << "Id     = " << id << endl;
+        cout << "nama   = " << id << endl;
+        cout << "Nilai  = " << id << endl;
+    }
+};
 int main(){
-    
+
 }
